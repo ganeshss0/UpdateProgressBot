57a5dcb7f33da064bb72d75c56ccb6693584b02d
Ganesh <gs000@proton.me>
2023-09-19 11:32:23 +0530

    Adding Files

--- handle_git.py
+++ handle_git.py
@@ -1,1 +1,14 @@
+import subprocess
 import os
+
+# Change directory to 'GitHub Repo'
+repo_directory = "my_data"
+os.chdir(repo_directory)
+
+# Run the Python script within 'GitHub Repo' (replace 'script.py' with your script's name)
+subprocess.run(["python", "visualization.py"])
+
+# Commit and push changes within 'GitHub Repo'
+subprocess.run(["git", "add", "."])
+subprocess.run(["git", "commit", "-m", "Update Progress"])
+subprocess.run(["git", "push", "origin", "main"])
